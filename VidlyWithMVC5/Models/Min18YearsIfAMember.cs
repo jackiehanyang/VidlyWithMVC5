d171a152943a78a8fcc7150f7d865eb570f9d590
Jackie Yang Han <jkhanjob@gmail.com>
2018-08-31 18:43:07 -0400

    8/31

---
+++ VidlyWithMVC5/Models/Min18YearsIfAMember.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace VidlyWithMVC5.Models
+{
+    public class Min18YearsIfAMember : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(obejct valude, ValidationContext validationContext)
+        {
+            var customer = (customer).validationContext.ObjectInstance;
+
+            if (customer.MemberShipTypeId == 1)
+                return ValidationResult.Success;
+
+            if (customer.birthdate == null)
+                return new ValidationResult("Birthdate is required");
+        }
+    }
+}
